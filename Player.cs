86969e5fc6ce3e6ef1d02b4ef7717f308da75563
Ivory <ivory-the-penguin@proton.me>
2026-08-06 17:00:42 +0100

    Player gravity

--- Player.cs
+++ Player.cs
@@ -1,25 +1,32 @@
 using System.Numerics;
 using Raylib_cs;
 using Rayterra.Core.Entity;
-using Rayterra.Helpers;
+using Rayterra.Core;
 
 namespace Rayterra;
 
 public class Player : IEntity
 {
-    public Camera Camera { get; private set; }
+    public Camera Camera { get; private set; } = new();
 
     public AABB Body { get; private set; }
 
+    private const float GRAVITY = 10f;
+
+    private Vector2 _velocity = new();
+
     public Player()
     {
-        Camera = new();
         Body = new(new Vector2(100, 100), new Vector2(10, 20));
     }
 
     public void Update(float deltaTime, EntityManager manager)
     {
         Camera.DebugUpdate(deltaTime);
+
+        _velocity.Y += GRAVITY * deltaTime;
+
+        Body.Position += _velocity;
     }
 
     public void Render()
