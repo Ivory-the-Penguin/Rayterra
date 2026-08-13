e4d544419ba5f79b0ef1262d8906788ce46ca2ba
Ivory <ivory-the-penguin@proton.me>
2026-08-13 17:46:24 +0100

    IsEmpty item fix

--- Item.cs
+++ Item.cs
@@ -4,7 +4,7 @@ namespace Rayterra;
 
 public record struct Item(TileID ID, uint Amount)
 {
-    public bool IsEmpty => ID >= 0 && Amount > 0;
+    public bool IsEmpty => ID < 0 && Amount == 0;
 
     public static Item None = new Item(TileID.None, 0);
 }
